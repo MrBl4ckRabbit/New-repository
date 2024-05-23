5e9440418bd7b29b6e615f9c0839e8c9ad8329dc
MrBl4ckRabbit <mr.bl4ck.rabbit@gmail.com>
2024-05-23 15:50:53 +0300

    Update methods

--- src/main/java/task_4/example_5/MyDataIoStreamRunner.java
+++ src/main/java/task_4/example_5/MyDataIoStreamRunner.java
@@ -1,13 +1,25 @@
 package task_4.example_5;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
 
 public class MyDataIoStreamRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         Person tom = new Person("Tom", 34, 1.68, false);
         writeDataToFile(tom);
         readDataFromFile();
+//        File file = new File("src/main/java/task_4/example_5/text_from.txt");
+//        Scanner scanner = new Scanner(file);
+//
+//        while (scanner.hasNext()) {
+//            System.out.println(scanner.nextLine());
+//        }
+
     }
 
     private static void writeDataToFile(Person person) {
@@ -37,6 +49,38 @@ public class MyDataIoStreamRunner {
         }
     }
 
+//    private static void readDataFromFile(){
+//        Path filePath = Paths.get("src/main/java/task_4/example_5/text_from.txt");
+//        try {
+//            String line = Files.readString(filePath);
+//            String[] values = line.split(" ");
+//
+//            String name = values[0];
+//            int age = Integer.parseInt(values[1]);
+//            double height = Double.parseDouble(values[2]);
+//            boolean married = Boolean.parseBoolean(values[3]);
+//
+//            System.out.printf("Name: %s  Age: %d  Height: %.2f  Married: %b",
+//                    name, age, height, married);
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
+
+//    private static void writeDataToFile(Person person) {
+//        Path filePath = Paths.get("src/main/java/task_4/example_5/text_to.txt");
+//
+//        try {
+//            Files.writeString(filePath, "");
+//            String data = String.format("%s %d %.2f %b%n",
+//                    person.name, person.age, person.height, person.married);
+//
+//            Files.writeString(filePath, data, StandardOpenOption.APPEND);
+//            System.out.println("File has been written");
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
 }
 
 
